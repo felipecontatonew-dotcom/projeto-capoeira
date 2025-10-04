22feff29c2bfd5868c4eb66b83614ba0092b15c5
fepemen <comercialmendesit@gmail.com>
2025-10-04 15:51:54 -0300

    Refatora para o padrao Application Factory

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
-# extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_socketio import SocketIO
 
-# Instancia as extensões que serão usadas no projeto.
 db = SQLAlchemy()
 login_manager = LoginManager()
+socketio = SocketIO()
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,12 @@
-from app import socketio as application
+from app import create_app
+from extensions import socketio
+
+# Cria a aplicação usando a fábrica
+app = create_app()
+
+# O objeto 'application' é o que o Gunicorn vai procurar e executar
+application = socketio
+
+# Bloco para rodar localmente (python wsgi.py)
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
